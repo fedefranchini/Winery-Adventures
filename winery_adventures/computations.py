6691d9338c612e1913c3a9a4ed650b3c6924a103
Federico Marras <196535660+MarrasFederico@users.noreply.github.com>
2026-08-27 18:10:58 +0200

    WA-06 establish abstract analyzer hierarchy

---
+++ winery_adventures/computations.py
@@ -0,0 +1,7 @@
+"""Definizione dell'analyzer dedicato ai calcoli ad alte prestazioni."""
+
+from winery_adventures.base import BaseWineryAnalyzer
+
+
+class WineryHPCComputations(BaseWineryAnalyzer):
+    """Raggruppa i calcoli numerici intensivi applicati alle letture."""
